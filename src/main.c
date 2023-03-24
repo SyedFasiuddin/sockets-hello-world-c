9384d420869574f496f88486eccfbed8993366b7
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-03-24 20:14:14 +0530

    echoing client

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <sys/un.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
 
 #define SOCKET_PATH "/tmp/socket-c-hello.socket"
 
@@ -43,6 +44,32 @@ void echo_server(void) {
 
     len = send(client_fd, buffer, len, 0);
     printf("Sent %d bytes\n", len);
+}
+
+void echo_client(void) {
+    int client_fd;
+    struct sockaddr_un server_addr = {
+        .sun_family = AF_UNIX,
+    };
+    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
+
+    if((client_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
+        log_err_die("Socket creation");
+
+    int server_fd;
+    if((server_fd = connect(client_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_un))) == -1)
+        log_err_die("connect");
+
+#define BUFFER_LEN 100
+    char input[BUFFER_LEN];
+    scanf("%s", input);
+
+    int len = write(client_fd, input, BUFFER_LEN);
+    printf("sent %d bytes\n", len);
+
+    char buffer[BUFFER_LEN];
+    len = recv(client_fd, buffer, BUFFER_LEN, 0);
+    printf("Recieved %d bytes\nMsg: %s", len, buffer);
 }
 
 int main(int argc, char **argv) {
